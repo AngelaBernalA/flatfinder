797ae8921bfe122b4c81c3ebae1930c74ca9b1bb
Angie Bernal <angiebernal@Angies-MacBook-Pro.local>
2024-09-22 13:30:44 -0700

    New flat CSS  added

--- src/app/components/flat-view/flat-view.component.ts
+++ src/app/components/flat-view/flat-view.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-flat-view',
   templateUrl: './flat-view.component.html',
   styleUrl: './flat-view.component.css'
 })
-export class FlatViewComponent {
+export class FlatViewComponent implements OnInit{
+  flat: any;
+
+  constructor(private authService: AuthService, private firestore: AngularFirestore) {}
+
+  ngOnInit(): void {
+    // Fetch all flats from Firestore
+    this.firestore.collection('flats').valueChanges().subscribe((flat: any[]) => {
+      this.flat = flat;
+    });
+  }
 
 }
